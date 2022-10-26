88df8791400e0d6dc6bcd5f47e46e46120429af9
TriptSharma <tript35.sharma@gmail.com>
2022-10-26 02:15:04 -0400

    add function descriptions and pseudo codes

--- Phase1/Wrapper.py
+++ Phase1/Wrapper.py
@@ -2,3 +2,12 @@
 # Karter Krueger and Tript Sharma
 # Wrapper.py
 
+import argparse
+import numpy as np
+import cv2
+import os
+
+# argparser = argparse.ArgumentParser()
+# argparser.add_argument('')
+# args = argparser.parse_args()
+
