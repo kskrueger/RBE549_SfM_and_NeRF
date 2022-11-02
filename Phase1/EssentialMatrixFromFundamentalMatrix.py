fix bugs in Linear Triangulation, Cam-pose disambiguation, E matrix calculation; add plot functions

Phase1/EssentialMatrixFromFundamentalMatrix.py
@@ -1,6 +1,7 @@
 # RBE549: Building Built in Minutes using SfM
 # Karter Krueger and Tript Sharma
 # EssentialMatrixFromFundamentalMatrix.py
+import numpy as np
 
 def essentialMatrixFromFundamentalMatrix(F,K):
     '''
@@ -9,5 +10,10 @@ def essentialMatrixFromFundamentalMatrix(F,K):
         K = Camera Intrinsics
     Output E
     E = K.T @ F @ K
+    
     '''
-    return K.T @ F @ K
+    E =  K.T @ F @ K
+    #cleanup E
+    U,D,VT = np.linalg.svd(E)
+    E_hat = U @ np.diag([1,1,0]) @ VT
+    return E_hat

Phase1/EstimateFundamentalMatrix.py
@@ -24,7 +24,6 @@ def estimateFundamentalMatrix(pt_correspondences):
     U*D*V^T = F (cleanup)
     '''
     # assert pt_correspondences.shape[0] == 8
-
     A = np.zeros((pt_correspondences.shape[0],9))
     
     def getRowOfA(row):
@@ -43,6 +42,7 @@ def estimateFundamentalMatrix(pt_correspondences):
     UF,UD,UV = np.linalg.svd(F_noisy)
     UD[-1]=0
     F = UF @ np.diag(UD) @ UV
+    F /= F[2,2]
     return F
 
 def estimateEpipole(F):
@@ -69,15 +69,15 @@ def plotEpipolarLines(F, x1, x2, img1, img2):
     F @ x1 = 0
     F.T @ x2 = 0 
     '''
-    x1 = np.hstack((x1, np.ones((x1.shape[0],1))))
-    l1 = F @ x1.T   #x1 size = (num_features, 3)
-    l1 /= l1[-1,:]
+    # x1 = np.hstack((x1, np.ones((x1.shape[0],1))))
+    # l1 = F @ x1.T   #x1 size = (num_features, 3)
+    # l1 /= l1[-1,:]
 
-    x2 = np.hstack((x2, np.ones((x2.shape[0],1))))
-    l2 = F.T @ x2.T
-    print(l2)
-    l2 /= l2[-1,:]
-    print(l2)
+    # x2 = np.hstack((x2, np.ones((x2.shape[0],1))))
+    # l2 = F.T @ x2.T
+    # print(l2)
+    # l2 /= l2[-1,:]
+    # print(l2)
 
     #get epipoles
     e1 = estimateEpipole(F)
@@ -85,9 +85,9 @@ def plotEpipolarLines(F, x1, x2, img1, img2):
 
     #draw lines
     for pt in x1:
-        i1 = cv2.line(img1, pt[:-1].astype(np.int), e1[:-1].astype(np.int), (0,0,0), 2)
+        i1 = cv2.line(img1, pt.astype(np.int), e1[:-1].astype(np.int), (0,0,0), 2)
     for pt in x2:
-        i2 = cv2.line(img2, pt[:-1].astype(np.int), e2[:-1].astype(np.int), (0,0,0), 2)
+        i2 = cv2.line(img2, pt.astype(np.int), e2[:-1].astype(np.int), (0,0,0), 2)
 
     cv2.imshow('1',i1)
     cv2.imshow('2',i2)

Phase1/ExtractCameraPose.py
@@ -23,9 +23,9 @@ def CamPoseFromE(E):
 
     U,D,V = np.linalg.svd(E)
     C = [U[:,-1], -U[:,-1]]
-    W = np.array([  [0,1,0],
+    W = np.array([  [ 0,1,0],
                     [-1,0,0],
-                    [0,0,1]
+                    [ 0,0,1]
                 ])
     R = [U@W@V, U@W.T@V]
 

Phase1/GetInliersRANSAC.py
@@ -3,12 +3,13 @@
 # GetInliersRANSAC.py
 import numpy as np
 from EstimateFundamentalMatrix import *
-def getInliersRANSAC(pt_correspondences, eps, n_iterations):
+def getInliersRANSAC(pt_correspondences, eps, n_iterations,K):
     '''
     Inputs:
         set of all point correspondences between two images
         eps: threshold for inlier check
         n_iterations: max num of iterations
+        K = camera intrinsic matrix
     Outputs:
         optimized F matrix
 
@@ -42,7 +43,7 @@ def getInliersRANSAC(pt_correspondences, eps, n_iterations):
             X1 = np.array([x1,y1,1])
             X2 = np.array([x2,y2,1])
             
-            val = X2.T @ F @ X1
+            val = X2 @ F @ X1
             # print(val)
             if (abs(val) < eps):
                 inlier_count+=1

Phase1/LinearTriangulation.py
@@ -43,10 +43,11 @@ def linearTriangulation(pt_pair_list, extrinsic_matrices, K):
     print(extrinsics1)
     C1, R1 = extrinsics1
     C2, R2 = extrinsics2
-    
-    P1 = K @ np.hstack((R1,C1.reshape((-1,1))))
+    I = np.eye(3)
 
-    P2 = K @ np.hstack((R2,C2.reshape((-1,1))))
+    P1 = K @ R1 @ np.hstack((I, -C1.reshape((-1,1))))
+
+    P2 = K @ R2 @ np.hstack((I, -C2.reshape((-1,1))))
 
     for x1i,x2i in zip(x1,x2):
         x1i_skew = skew(x1i)
@@ -59,5 +60,5 @@ def linearTriangulation(pt_pair_list, extrinsic_matrices, K):
         Xi = V[-1,:]
         Xi /= Xi[-1]
         X.append(Xi)
-    print(X)
+    # print(X)
     return np.array(X)