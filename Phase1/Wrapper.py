2630444805004e5d247c59817cc008d9f90ab6ff
Karter Krueger <krueger.karter@gmail.com>
2022-11-12 22:32:12 -0500

    Full SfM works, with exception of bundle adjustment.

--- Phase1/Wrapper.py
+++ Phase1/Wrapper.py
@@ -19,7 +19,7 @@ from NonlinearTriangulation import nonlinear_triangulate
 from DisambiguateCameraPose import disambiguate_pose
 from PnPRANSAC import ransac_pnp
 from ExtractCameraPose import get_camera_poses
-from plotting import plotEpipolarLines
+from plotting import plotEpipolarLines, plot_camera
 from plotting import plot_matches
 from scipy.spatial.transform import Rotation
 
@@ -29,7 +29,7 @@ matplotlib.use("MacOSX")
 import matplotlib.pyplot as plt
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--data_path", default="./Data/UMD")
+argparser.add_argument("--data_path", default="./Data/P3Data")
 args = argparser.parse_args()
 
 data_path = args.data_path
@@ -41,7 +41,7 @@ match_paths = glob.glob(os.path.join(data_path, "matching*.txt"))
 calibration_file = open(calibration_path, 'r')
 
 K = calibration_data = np.array([l.split(' ') for l in calibration_file], dtype=float)
-imgs = [cv2.imread(img_path) for img_path in img_paths]
+imgs = [cv2.imread(img_path) for img_path in sorted(img_paths)]
 match_files = sorted([(int(m_path.split('matching')[-1].replace('.txt', '')), [l for l in open(m_path, 'r')]) for m_path in
                match_paths])
 
@@ -50,6 +50,10 @@ match_data = {}
 image_visbilities = {}
 running_feature_idx_start = 0
 
+file_start = 0
+all_m_features = []
+visible_lines = []
+pts_lines = []
 for m_i, m_l in match_files:
     m_i = int(m_i)
     match_nfeatures[m_i] = int(m_l[0].split('nFeatures: ')[-1])
@@ -63,6 +67,10 @@ for m_i, m_l in match_files:
         R = int(R)
         B = int(B)
         G = int(G)
+        visible_line = np.zeros((len(imgs)), dtype=int)
+        visible_line[m_i-1] = 1
+        pts_line = np.zeros((len(imgs), 2), dtype=float)
+        pts_line[m_i-1, :] = u_i, v_i
         for k in range(num_matches-1):
             start = 6 + k*3
             img_j, u_j, v_j = vals[start:start+3]
@@ -70,241 +78,229 @@ for m_i, m_l in match_files:
             img_j = int(img_j)
             if img_j not in features.keys():
                 features[img_j] = []
-            features[img_j].append((num_matches, R, G, B, u_i, v_i, img_j, u_j, v_j, feature_k + running_feature_idx_start))
-            running_feature_idx_start = feature_k
+            features[img_j].append((m_i, R, G, B, u_i, v_i, img_j, u_j, v_j, feature_k + running_feature_idx_start))
+            all_m_features.append((m_i, R, G, B, u_i, v_i, img_j, u_j, v_j, feature_k + running_feature_idx_start + file_start))
             visible_from_img_i[img_j-1, feature_k] = 1
+            visible_line[img_j - 1] = 1
+            pts_line[img_j - 1, :] = u_j, v_j
+        running_feature_idx_start = feature_k
+        visible_lines.append(visible_line)
+        pts_lines.append(pts_line)
 
     visible_from_img_i[m_i-1, :] = 1
     image_visbilities[m_i] = visible_from_img_i
     match_data[m_i] = features
+    file_start = running_feature_idx_start
 
+all_data = np.array(all_m_features)
+new_visible = np.stack(visible_lines).astype(int)
+new_x = np.stack(pts_lines).astype(float)
 
 def get_homogeneous(pts):
     return np.hstack((pts, np.ones((len(pts), 1))))
 
+# visibility_matrix0 = np.hstack([v for v in image_visbilities.values()])
+# visibility_matrix = np.zeros_like(visibility_matrix0)
 
-visibility_matrix = np.hstack([v for v in image_visbilities.values()])
+# Step 1: Prune image features (already done before reading in the files)
 
-for img_ii in match_data.keys():
-    for img_jj in match_data[img_ii].keys():
-        print("I, J", img_ii, img_jj)
-        data = np.array(match_data[img_ii][img_jj])
-        pts1 = data[:, 4:6]
-        pts2 = data[:, 7:9]
-        feature_indexes = data[:, -1].astype(int)
+# for img_ii in match_data.keys():
+    # for img_jj in match_data[img_ii].keys():
 
+# Step 2: Compute F and E for features between images 1 and 2
+img_ii = 1
+img_jj = 4
 
-        plot_matches(imgs[0], imgs[1], pts1, pts2)
+allC = []
+allR = []
+allX = np.zeros((len(new_x), 3))
+# new_visible = np.zeros((len(m), len(imgs)))
+camOrder = [img_ii, img_jj]
+good_idx = np.zeros((len(new_x), 1), dtype=int)
 
-        # img_gray = cv2.cvtColor(imgs[0], cv2.COLOR_BGR2GRAY)
-        # sift = cv2.SIFT_create()
-        # kp = sift.detect(img_gray, None)
+# all_match_data = np.vstack([np.vstack([v for v in match_data[img_k].values()]) for img_k in match_data.keys()])
+# all_img1_idx = m[:, 0].astype(int)
+# all_img2_idx = m[:, 6].astype(int)
 
-        # a = cv2.goodFeaturesToTrack(img_gray, 1000, .01, 5)
-        # img_out = imgs[0].copy()
-        # for i in range(len(a)):
-        #     cv2.circle(img_out, a[i, 0, :].astype(int), 5, (0, 255, 0), -1)
-        # cv2.imshow("img_out", img_out)
-        # cv2.waitKey(0)
+inliers_ij = np.where((new_visible[:, img_ii-1]) & (new_visible[:, img_jj-1]))[0]
 
-        # img = cv2.drawKeypoints(img_gray,kp,imgs[0])
-        # cv2.imshow("Sift", img)
-        # cv2.waitKey(0)
+print("I, J", img_ii)
+# data = np.array(match_data[img_ii][img_jj])
+# pts1 = data[:, 4:6]
+# pts2 = data[:, 7:9]
+pts1 = new_x[inliers_ij, img_ii-1]
+pts2 = new_x[inliers_ij, img_jj-1]
+feature_indexes = inliers_ij.astype(int) #data[:, -1].astype(int)
 
+plot_matches(imgs[img_ii-1], imgs[img_jj-1], pts1, pts2)
 
-        width, height = imgs[0].shape[:2]
-        pts1h = get_homogeneous(pts1)
-        pts2h = get_homogeneous(pts2)
+width, height = imgs[0].shape[:2]
+pts1h = get_homogeneous(pts1)
+pts2h = get_homogeneous(pts2)
 
-        ptsT = np.array([[1 / width, 0, -1],
-                         [0, 1 / height, -1],
-                         [0, 0, 1]])
-        ptsT = np.linalg.inv(K)
+# ptsT = np.array([[1 / width, 0, -1],
+#                  [0, 1 / height, -1],
+#                  [0, 0, 1]])
+ptsT = np.linalg.inv(K)
 
-        K_inv = np.linalg.inv(calibration_data)
-        pts1nh = (ptsT @ pts1h.T).T
-        pts2nh = (ptsT @ pts2h.T).T
-        pts1n = pts1nh[:, :2]
-        pts2n = pts2nh[:, :2]
+K_inv = np.linalg.inv(calibration_data)
+pts1nh = (ptsT @ pts1h.T).T
+pts2nh = (ptsT @ pts2h.T).T
+pts1n = pts1nh[:, :2]
+pts2n = pts2nh[:, :2]
 
-        # Fcv, Fcv_inliers = cv2.findFundamentalMat(pts1, pts2, cv2.FM_RANSAC, .3, 1, 100)
+pts1nh = get_homogeneous(pts1n)
+pts2nh = get_homogeneous(pts2n)
 
-        pts1nh = get_homogeneous(pts1n)
-        pts2nh = get_homogeneous(pts2n)
+F2, F2_inliers = ransac_F(pts1n, pts2n, .25, 5000, imgs)
 
-        # inlier_errors = np.zeros((len(pts1nh), 1))
-        # for j in range(len(pts1nh)):
-        #     inlier_errors[j] = pts2nh[j].T @ (Fcv @ pts1nh[j])
-        # inliers_idx = np.abs(inlier_errors) < .3
+F2un = np.dot(ptsT.T, np.dot(F2, ptsT))
+F2un /= F2un[2, 2]
+F = F2un
+print("F", F)
 
-        # TODO: try without normalized
-        F2, F2_inliers = ransac_F(pts1n, pts2n, .001, 1000, imgs)
-        # F2, F2_inliers = ransac_F(pts1, pts2, .005, 250, imgs)
+F2_inliers = F2_inliers[:, 0]
+feature_indexes_i = feature_indexes[F2_inliers]
+good_idx[feature_indexes_i] = 1
+pts1i = pts1[F2_inliers]
+pts2i = pts2[F2_inliers]
+pts1hi = pts1h[F2_inliers]
+pts2hi = pts2h[F2_inliers]
+pts1nhi = pts1nh[F2_inliers]
+pts2nhi = pts2nh[F2_inliers]
+pts1ni = pts1n[F2_inliers]
+pts2ni = pts2n[F2_inliers]
 
-        # F2, F2_inliers = cv2.findFundamentalMat(pts1, pts2, cv2.FM_RANSAC, .3, 1, 250)
+plot_matches(imgs[img_ii-1], imgs[img_jj-1], pts1i, pts2i, "Matches F_Inlier")
 
-        # TODO: try without normalized
-        F2un = np.dot(ptsT.T, np.dot(F2, ptsT))
-        # F2un = F2
-        F2un /= F2un[2, 2]
-        F = F2un
-        print("F", F)
+E = get_E_from_F(K, F)
+print("E", E)
 
-        F2_inliers = F2_inliers[:, 0]  # .astype(bool)
-        feature_indexes_i = feature_indexes[F2_inliers]
-        pts1i = pts1[F2_inliers]
-        pts2i = pts2[F2_inliers]
-        pts1hi = pts1h[F2_inliers]
-        pts2hi = pts2h[F2_inliers]
-        pts1nhi = pts1nh[F2_inliers]
-        pts2nhi = pts2nh[F2_inliers]
-        pts1ni = pts1n[F2_inliers]
-        pts2ni = pts2n[F2_inliers]
+cam_poses = get_camera_poses(E)
 
-        plot_matches(imgs[0], imgs[1], pts1i, pts2i)
+cam_pose, lin_X, all_X = disambiguate_pose(None, cam_poses, pts1hi, pts2hi, K)
+print("DisambiguateCameraPose", cam_pose[0])
+allC.append(cam_pose[0])
+allR.append(cam_pose[1])
 
-        # E = get_E_from_F(K, F2)
-        E = get_E_from_F(K, F)
-        print("E", E)
+# plot_3d(all_X[0])
+# plot_3d(all_X[1])
+# plot_3d(all_X[2])
+# plot_3d(all_X[3])
 
-        # EE = K.T @ Fcv @ K
+# plt.scatter(all_X[0][:, 0], all_X[0][:, 2], s=2, c='g')
+# plt.scatter(all_X[1][:, 0], all_X[1][:, 2], s=2, c='r')
+# plt.scatter(all_X[2][:, 0], all_X[2][:, 2], s=2, c='b')
+# plt.scatter(all_X[3][:, 0], all_X[3][:, 2], s=2, c='k')
+# plt.show()
 
-        cam_poses = get_camera_poses(E)
+P0 = (np.zeros((3, 1)), np.eye(3))
+nonlin_X = nonlinear_triangulate(lin_X, P0, cam_pose, pts1i, pts2i, K)
+allX[good_idx[:, 0] == 1] = nonlin_X
 
-        cam_pose, lin_X, all_X = disambiguate_pose(None, cam_poses, pts1hi, pts2hi, K)
-        from delete_helper import DisambiguateCameraPose
+# plt.scatter(lin_X[:, 0], lin_X[:, 2], s=2, c='r', label='Linear X')
+# plt.scatter(nonlin_X[:, 0], nonlin_X[:, 2], s=2, c='g', label='NonLinear X')
+# plt.legend()
+# plt.show()
 
-        # camX, camR, camC = DisambiguateCameraPose([c[0] for c in cam_poses], [c[1] for c in cam_poses], all_X)
-        # lin_X = camX
-        # cam_pose = (camC, camR)
-        print("DisambiguateCameraPose", cam_pose[0])
+P1 = np.dot(K, np.dot(P0[1], np.hstack((np.eye(3), -P0[0]))))
+P2 = np.dot(K, np.dot(cam_poses[0][1], np.hstack((np.eye(3), -cam_poses[0][0]))))
 
+X = np.hstack((nonlin_X, np.ones((len(nonlin_X), 1))))
+u1 = np.divide((np.dot(P1[0, :], X.T).T), (np.dot(P1[2, :], X.T).T))
+v1 = np.divide((np.dot(P1[1, :], X.T).T), (np.dot(P1[2, :], X.T).T))
 
-        def plot_3d(X):
-            fig = plt.figure()
-            ax = fig.add_subplot(projection='3d')
+img_copy = imgs[0].copy()
+for u, v in zip(u1, v1):
+    cv2.circle(img_copy, (int(u), int(v)), 3, (0, 255, 0), -1)
 
-            ax.scatter(X[:, 0], X[:, 2], X[:, 1])
+for u, v in pts1i:
+    cv2.circle(img_copy, (int(u), int(v)), 2, (0, 0, 255), -1)
 
-            ax.set_xlim(-6, 8)
-            ax.set_ylim(2, 18)
-            ax.set_zlim(-10, 10)
+cv2.imshow("NonLinearX Reprojection", img_copy)
+# cv2.waitKey(0)
 
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Z Label')
-            ax.set_zlabel('Y Label')
-            plt.show()
+plt.figure()
+plt.scatter(nonlin_X[:, 0], nonlin_X[:, 2], s=2, c='g', label='NonLinear X')
+colors = ['b', 'r', 'k', 'purple', 'pink', 'orange']
 
+eulers = Rotation.from_matrix(allR[0]).as_euler('xyz')
+plt.plot(allC[0][0],
+         allC[0][2],
+         marker=(3, 0, int(eulers[1])),
+         markersize=15,
+         linestyle='None',
+         c=colors[img_ii],
+         label="Camera ".format(img_ii))
 
-        # plot_3d(all_X[0])
-        # plot_3d(all_X[1])
-        # plot_3d(all_X[2])
-        # plot_3d(all_X[3])
+# PnP applied to indexes 3-I
+for img_jj in match_data[img_ii].keys():
+    if img_jj in camOrder:
+        print("Already processed cam")
+        continue
+    # data = np.array(match_data[img_ii][img_jj])
+    inliers_ij2 = np.where((new_visible[:, img_ii-1]) & (new_visible[:, img_jj-1]) & good_idx[:, 0])[0]
 
-        # plt.scatter(all_X[0][:, 0], all_X[0][:, 2], s=2, c='g')
-        # plt.scatter(all_X[1][:, 0], all_X[1][:, 2], s=2, c='r')
-        # plt.scatter(all_X[2][:, 0], all_X[2][:, 2], s=2, c='b')
-        # plt.scatter(all_X[3][:, 0], all_X[3][:, 2], s=2, c='k')
-        # plt.show()
+    print("I, J", img_ii)
+    pts_i = new_x[inliers_ij2, img_ii-1]
+    pts_j = new_x[inliers_ij2, img_jj-1]
+    feature_indexes_j = inliers_ij2.astype(int)
+    x_inliers = allX[feature_indexes_j]
+    pnp_P, pnp_C, pnp_R, pnp_inliers = ransac_pnp(pts_j, x_inliers, K, 50, 1000)
 
-        P0 = (np.zeros((3, 1)), np.eye(3))
-        nonlin_X = nonlinear_triangulate(lin_X, P0, cam_pose, pts1i, pts2i, K)
+    feature_indexes_pnp_j = feature_indexes_j[pnp_inliers]
+    # visibility_matrix[img_ii-1, feature_indexes_pnp_j] = 1
+    # visibility_matrix[img_jj-1, feature_indexes_pnp_j] = 1
+    pts_j_pnpi = pts_j[pnp_inliers]
+    X_pnpi = x_inliers[pnp_inliers]
 
-        plt.scatter(lin_X[:, 0], lin_X[:, 2], s=2, c='r', label='Linear X')
-        plt.scatter(nonlin_X[:, 0], nonlin_X[:, 2], s=2, c='g', label='NonLinear X')
-        plt.legend()
-        # plt.show()
+    pnp_nl_C, pnp_nl_R, pnp_nl_P = nonlinear_pnp(pnp_C, pnp_R, pts_j_pnpi, X_pnpi, K)
+    camOrder.append(img_jj)
+    allR.append(pnp_nl_R)
+    allC.append(pnp_nl_C)
 
-        # from delete_helper import NonLinearTriangulation
-        # nonTri = NonLinearTriangulation(K, pts1i, pts2i, pts_X_lin, np.eye(3), np.zeros((3, 1)), cam_poses[0][1], cam_poses[0][0])
+    eulers = Rotation.from_matrix(pnp_nl_R).as_euler('xyz')
+    plt.plot(pnp_nl_C[0],
+             pnp_nl_C[2],
+             marker=(3, 0, int(eulers[1])),
+             markersize=15,
+             linestyle='None',
+             c=colors[img_jj],
+             label="Camera {}".format(img_jj))
 
+    # plot_camera(pnp_R, pnp_nl_R, nonlin_X, pnp_C, pnp_nl_C)
 
-        P1 = np.dot(K, np.dot(P0[1], np.hstack((np.eye(3), -P0[0]))))
-        P2 = np.dot(K, np.dot(cam_poses[0][1], np.hstack((np.eye(3), -cam_poses[0][0]))))
+    # plt.figure()
+    # plt.scatter(X_pnpi[:, 0], X_pnpi[:, 2], s=2)
+    # plt.show()
 
-        X = np.hstack((nonlin_X, np.ones((len(nonlin_X), 1))))
-        u1 = np.divide((np.dot(P1[0, :], X.T).T), (np.dot(P1[2, :], X.T).T))
-        v1 = np.divide((np.dot(P1[1, :], X.T).T), (np.dot(P1[2, :], X.T).T))
+    Xh_pnpi = np.hstack((X_pnpi, np.ones((len(X_pnpi), 1))))
+    u1pnp = np.divide((np.dot(pnp_P[0, :], Xh_pnpi.T).T), (np.dot(pnp_P[2, :], Xh_pnpi.T).T))
+    v1pnp = np.divide((np.dot(pnp_P[1, :], Xh_pnpi.T).T), (np.dot(pnp_P[2, :], Xh_pnpi.T).T))
 
-        img_copy = imgs[0].copy()
-        for u, v in zip(u1, v1):
-            cv2.circle(img_copy, (int(u), int(v)), 3, (0, 255, 0), -1)
+    u1pnp_nl = np.divide((np.dot(pnp_nl_P[0, :], Xh_pnpi.T).T), (np.dot(pnp_nl_P[2, :], Xh_pnpi.T).T))
+    v1pnp_nl = np.divide((np.dot(pnp_nl_P[1, :], Xh_pnpi.T).T), (np.dot(pnp_nl_P[2, :], Xh_pnpi.T).T))
 
-        for u, v in pts1i:
-            cv2.circle(img_copy, (int(u), int(v)), 2, (0, 0, 255), -1)
+    img_copy = imgs[img_ii].copy()
+    for u, v in pts_j_pnpi:
+        cv2.circle(img_copy, (int(u), int(v)), 2, (0, 0, 255), -1)
 
-        cv2.imshow("NonLinearX Reprojection", img_copy)
+    for u, v in zip(u1pnp, v1pnp):
+        cv2.circle(img_copy, (int(u), int(v)), 3, (0, 255, 0), -1)
 
-        # from delete_helper import skew, compute_epipole, linear_triangulation_hartley
-        # e = compute_epipole(F.T)  # left epipole
-        # Te = skew(e)
-        # pp = np.vstack((np.dot(Te, F.T).T, e)).T
-        # lt = linear_triangulation_hartley(pts1hi.T, pts2hi.T, np.hstack((np.eye(3), np.zeros((3, 1)))), pp).T[:, :3]
-        # plt.scatter(lt[:, 0], lt[:, 2])
+    for u, v in zip(u1pnp_nl, v1pnp_nl):
+        cv2.circle(img_copy, (int(u), int(v)), 3, (255, 0, 255), -1)
 
-        pnp_P, pnp_C, pnp_R, pnp_inliers = ransac_pnp(pts1i, nonlin_X, K, 25, 1000)
+    cv2.imshow("PnP Reprojections on IMG {}-{} : (Original=Red, LinearPnP=Green, NonLinearPnP=Purple)".format(img_ii, img_jj), img_copy)
+    # cv2.waitKey(0)
 
-        feature_indexes_pnpi = feature_indexes_i[pnp_inliers]
-        visibility_matrix[img_ii-1, feature_indexes_pnpi] = 2
-        visibility_matrix[img_jj-1, feature_indexes_pnpi] = 2
-        pts1i_pnpi = pts1i[pnp_inliers]
-        X_pnpi = nonlin_X[pnp_inliers]
-
-        pnp_nl_C, pnp_nl_R, pnp_nl_P = nonlinear_pnp(pnp_C, pnp_R, pts1i_pnpi, X_pnpi, K)
-
-        eulers_lin = Rotation.from_matrix(pnp_R).as_euler('xyz')
-        eulers = Rotation.from_matrix(pnp_nl_R).as_euler('xyz')
-
-        plt.figure()
-        plt.scatter(nonlin_X[:, 0], nonlin_X[:, 2], s=2, c='g', label='NonLinear X')
-
-        plt.plot(pnp_C[0],
-                 pnp_C[2],
-                 marker=(3, 0, int(eulers_lin[1])),
-                 markersize=15,
-                 linestyle='None',
-                 c='green',
-                 label="Camera LinearPnP")
-
-        plt.plot(pnp_nl_C[0],
-                 pnp_nl_C[2],
-                 marker=(3, 0, int(eulers[1])),
-                 markersize=15,
-                 linestyle='None',
-                 c='purple',
-                 label="Camera")
-        plt.legend()
-        # plt.show()
-
-        # plt.figure()
-        # plt.scatter(X_pnpi[:, 0], X_pnpi[:, 2], s=2)
-        # plt.show()
-
-        Xh_pnpi = np.hstack((X_pnpi, np.ones((len(X_pnpi), 1))))
-        u1pnp = np.divide((np.dot(pnp_P[0, :], Xh_pnpi.T).T), (np.dot(pnp_P[2, :], Xh_pnpi.T).T))
-        v1pnp = np.divide((np.dot(pnp_P[1, :], Xh_pnpi.T).T), (np.dot(pnp_P[2, :], Xh_pnpi.T).T))
-
-        u1pnp_nl = np.divide((np.dot(pnp_nl_P[0, :], Xh_pnpi.T).T), (np.dot(pnp_nl_P[2, :], Xh_pnpi.T).T))
-        v1pnp_nl = np.divide((np.dot(pnp_nl_P[1, :], Xh_pnpi.T).T), (np.dot(pnp_nl_P[2, :], Xh_pnpi.T).T))
-
-        img_copy = imgs[0].copy()
-        for u, v in pts1i_pnpi:
-            cv2.circle(img_copy, (int(u), int(v)), 2, (0, 0, 255), -1)
-
-        for u, v in zip(u1pnp, v1pnp):
-            cv2.circle(img_copy, (int(u), int(v)), 3, (0, 255, 0), -1)
-
-        for u, v in zip(u1pnp_nl, v1pnp_nl):
-            cv2.circle(img_copy, (int(u), int(v)), 3, (255, 0, 255), -1)
-
-        cv2.imshow("PnP Reprojections (Original=Red, LinearPnP=Green, NonLinearPnP=Purple)", img_copy)
-        # cv2.waitKey(0)
-
-        print("Finished, i, j", img_ii, img_jj)
+    print("Finished, i, j", img_ii, img_jj)
 
 # visibility =
 # scipy.optimize.least_squares()
 
+plt.legend()
+plt.plot()
 print("a")
 
 # lns = plotEpipolarLines(F2un, pts1i, pts2i, imgs[0], imgs[1])
